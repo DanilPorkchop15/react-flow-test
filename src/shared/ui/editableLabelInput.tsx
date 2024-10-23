38d7a4802b76c8d7a3dff81dd4cc280a8a2fd815
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-23 17:53:53 +0300

    feat(shared): add editable label input

---
+++ src/shared/ui/editableLabelInput.tsx
@@ -0,0 +1,39 @@
+import React, {memo, useCallback, useRef} from "react";
+
+export const EditableLabelInput = memo<{
+  label: string;
+  value: string;
+  onInputChange: (value: string) => void;
+  onLabelChange: (label: string) => void;
+}>(({ label, value, onInputChange, onLabelChange }) => {
+  const labelRef = useRef<HTMLLabelElement>(null);
+
+  const handleLabelBlur = useCallback(
+    (e: React.FocusEvent<HTMLLabelElement>) => {
+      onLabelChange(e.currentTarget.textContent || '');
+    },
+    [onLabelChange]
+  );
+
+  return (
+    <>
+      <label
+        ref={labelRef}
+        contentEditable
+        suppressContentEditableWarning
+        className="nodrag"
+        onBlur={handleLabelBlur}
+      >
+        {label}
+      </label>
+      <br />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onInputChange(e.target.value)}
+        className="nodrag"
+        style={{ border: '1px solid black' }}
+      />
+    </>
+  );
+});
