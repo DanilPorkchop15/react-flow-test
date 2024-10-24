90fd6324f9fdaea3547c1b227d76df38f2fb8315
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 10:37:07 +0300

    fix(flow): fix behavior

--- src/widgets/scalableMap/lib/getConnectedNodes.ts
+++ src/widgets/scalableMap/lib/getConnectedNodes.ts
@@ -1,16 +1,13 @@
 import {Edge, Node} from "@xyflow/react";
 
-export const getConnectedNodes = ( nodes: Node[], edges: Edge[]) => {
-  const connectedNodes: Node[] = [];
-  const connectedEdges: Edge[] = [];
-  nodes.forEach((node) => {
-    edges.forEach((edge) => {
-      if (edge.source === node.id || edge.target === node.id) {
-        connectedNodes.push(node);
-        connectedEdges.push(edge);
-      }
-    });
-  });
+export const getConnectedNodes = (nodes: Node[], edges: Edge[]) => {
+  const nodeIds = nodes.map((node) => node.id);
+  const connectedEdges = edges.filter(
+    (edge) => nodeIds.includes(edge.source) || nodeIds.includes(edge.target)
+  );
+  const connectedNodes = nodes.filter((node) =>
+    connectedEdges.some((edge) => edge.source === node.id || edge.target === node.id)
+  );
   return {
     nodes: connectedNodes,
     edges: connectedEdges,
