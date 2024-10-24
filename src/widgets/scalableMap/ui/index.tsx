e26d1f4094e9328736fc356330eb05af91a96998
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 10:30:46 +0300

    feat(flow): implement api request

--- src/widgets/scalableMap/ui/index.tsx
+++ src/widgets/scalableMap/ui/index.tsx
@@ -10,15 +10,15 @@ import {
   useReactFlow
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import {initialNodes, nodeTypes} from "../config";
+import {initialEdges, initialNodes, nodeTypes} from "../config";
 import {useCallback} from "react";
 import {AddNodesPanelFeature} from "../../../features/panel";
 import {FlowApi} from "../api";
 
 export function ScalableMapWidget() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const {getNodes} = useReactFlow()
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const {getNodes, getEdges} = useReactFlow();
 
   const onConnect = useCallback(
     (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
@@ -26,7 +26,7 @@ export function ScalableMapWidget() {
   );
 
   return (
-    <div style={{height: '80vh', width: '100%', border: '4px dotted black'}}>
+    <div style={{ height: '80vh', width: '100%', border: '4px dotted black' }}>
       <ReactFlow
         nodeTypes={nodeTypes}
         nodes={nodes as Node[]}
@@ -35,14 +35,16 @@ export function ScalableMapWidget() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
-        minZoom={0.1}
+        minZoom={0.5}
         maxZoom={2}
       >
-        <Background bgColor="#ffe"/>
-        <Controls/>
-        <AddNodesPanelFeature setNodes={setNodes}/>
+        <Background bgColor="#ffe" />
+        <Controls />
+        <AddNodesPanelFeature setNodes={setNodes} />
       </ReactFlow>
-      <button onClick={() => FlowApi.PostNodes(getNodes())}>REQUEST</button>
+      <button onClick={() => FlowApi.PostNodes({ nodes: getNodes(), edges: getEdges() })}>
+        REQUEST
+      </button>
     </div>
   );
 }
