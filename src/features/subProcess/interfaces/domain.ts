b9d7ebc0e264a9a306bb9011c4a8ce41643cff6d
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 11:04:49 +0300

    fix(nodes): add data types

---
+++ src/features/subProcess/interfaces/domain.ts
@@ -0,0 +1,14 @@
+export type SubProcessNodeData = {
+  field1?: {
+    text?: string;
+    label?: string;
+  };
+  field2?: {
+    text?: string;
+    label?: string;
+  };
+  field3?: {
+    text?: string;
+    label?: string;
+  };
+}
--- src/features/subProcess/ui/index.tsx
+++ src/features/subProcess/ui/index.tsx
@@ -1,38 +1,32 @@
 import {memo, useCallback, useEffect, useState} from 'react';
-import {Handle, NodeProps, Position, useReactFlow} from '@xyflow/react';
-import {EditableLabelInput, NodeWrapper} from "../../shared/ui";
+import {Handle, Node, NodeProps, Position, useReactFlow} from '@xyflow/react';
+import {EditableLabelInput, NodeWrapper} from "../../../shared/ui";
+import {SubProcessNodeData} from "../interfaces";
 
-type SubProcessNodeProps = NodeProps & {
-  data: {
-    field1?: {
-      text?: string;
-      label?: string;
-    },
-    field2?: {
-      text?: string;
-      label?: string;
-    }
-  };
-};
+type SubProcessNodeProps = NodeProps<Node<SubProcessNodeData>>
 
-export const ProcessNode = memo(({data, id, type}: SubProcessNodeProps) => {
+export const SubProcessNode = memo(({data, id, type}: SubProcessNodeProps) => {
+
   const {setNodes} = useReactFlow();
 
   const field1Text = data?.field1?.text || 'Field 1';
   const field2Text = data?.field2?.text || 'Field 2';
+  const field3Text = data?.field3?.text || 'Field 3';
 
   const field1Label = data?.field1?.label || 'Field 1';
   const field2Label = data?.field2?.label || 'Field 2';
+  const field3Label = data?.field3?.label || 'Field 3';
 
   const [field1, setField1] = useState(field1Text);
   const [field2, setField2] = useState(field2Text);
+  const [field3, setField3] = useState(field3Text);
 
   const [label1, setLabel1] = useState(field1Label);
   const [label2, setLabel2] = useState(field2Label);
-
+  const [label3, setLabel3] = useState(field3Label);
 
   const updateNodeData = useCallback(
-    (field1Text: string, field1Label: string, field2Text: string, field2Label: string) => {
+    (field1Text: string, field1Label: string, field2Text: string, field2Label: string, field3Text: string, field3Label: string) => {
       setNodes((nodes) =>
         nodes.map((node) => {
           if (node.id === id) {
@@ -42,6 +36,7 @@ export const ProcessNode = memo(({data, id, type}: SubProcessNodeProps) => {
                 ...node.data,
                 field1: {text: field1Text, label: field1Label},
                 field2: {text: field2Text, label: field2Label},
+                field3: {text: field3Text, label: field3Label},
               },
             };
           }
@@ -53,8 +48,9 @@ export const ProcessNode = memo(({data, id, type}: SubProcessNodeProps) => {
   );
 
   useEffect(() => {
-    updateNodeData(field1, label1, field2, label2);
-  }, [field1, label1, field2, label2, updateNodeData]);
+    updateNodeData(field1, label1, field2, label2, field3, label3);
+  }, [field1, label1, field2, label2, field3, label3, updateNodeData]);
+
 
   return (
     <NodeWrapper>
@@ -73,8 +69,15 @@ export const ProcessNode = memo(({data, id, type}: SubProcessNodeProps) => {
           onInputChange={setField2}
           onLabelChange={setLabel2}
         />
+        <br/>
+        <EditableLabelInput
+          label={label3}
+          value={field3}
+          onInputChange={setField3}
+          onLabelChange={setLabel3}
+        />
       </div>
-      <Handle type="source" position={Position.Bottom} id="process"/>
+      <Handle type="target" position={Position.Top} id="subPlibrocess"/>
     </NodeWrapper>
   );
 });
