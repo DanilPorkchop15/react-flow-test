28e84ef2733e105176c74f8aab6a33e7ffd2ed9a
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 10:30:00 +0300

    feat(flow): add pseudo request

--- src/widgets/scalableMap/api/api.ts
+++ src/widgets/scalableMap/api/api.ts
@@ -1,5 +1,23 @@
+import {getConnectedNodes} from "../lib";
+import {PostNodesDto} from "../interfaces";
+
 export class FlowApi {
-  public static PostNodes = (data: unknown) =>{
-    console.log(JSON.stringify(data, null, 1));
+  public static  PostNodes = async ({nodes, edges} : PostNodesDto) =>{
+    const data = getConnectedNodes(nodes, edges);
+    console.log("Connected nodes", data);
+    try {
+      const response = await fetch("https://test/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 }
